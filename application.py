1ce78bed326a637bf16af500ce3b210fc0c2be8e
tales rodrigues jardim <talesjardim@gmail.com>
2021-09-30 00:28:52 -0300

    stable version
    all check50 pass, project send to EDX

--- application.py
+++ application.py
@@ -202,17 +202,17 @@ def register():
     if request.method == "POST":
          # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
-        elif not request.form.get("confirmationPassword"):
-            return apology("must Confirm the password", 403)
+        elif not request.form.get("confirmation"):
+            return apology("must Confirm the password", 400)
 
-        elif request.form.get("confirmationPassword") != request.form.get("password"):
-            return apology("The password must be the same", 403)
+        elif request.form.get("confirmation") != request.form.get("password"):
+            return apology("The password must be the same", 400)
 
         name = request.form.get("username")
         password = request.form.get("password")
@@ -223,8 +223,8 @@ def register():
         rows = db.execute("SELECT * FROM users WHERE username = ?", name)
 
         # Ensure username exists and password is correct
-        if len(rows) == 1:
-            return apology("The user alredy exist", 403)
+        if len(rows) >= 1:
+            return apology("The user alredy exist", 400)
         
          # Inserting users into table users
         rows = db.execute("INSERT INTO users(username, hash, cash) VALUES(?, ?, ?)", name, passwordHash, 10000)
